2f364b2b3c787c26edc988f0a631601b87f726c0
paneee <dudczyk@o2.pl>
2021-12-11 15:16:03 +0100

    new api - first commit

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -6,7 +6,7 @@ String httpPrefix = "http://";
 
 Future<List<WebRadio>> getRadios(String ip, String port) async {
   Uri getAllStationPath =
-      Uri.parse(httpPrefix + ip + ':' + port + '/api/getAllStation/');
+      Uri.parse(httpPrefix + ip + ':' + port + '/api/radio/getAllStation/');
   final response = await http.get(getAllStationPath);
 
   if (response.statusCode == 200) {
@@ -18,7 +18,7 @@ Future<List<WebRadio>> getRadios(String ip, String port) async {
 
 Future<WebRadio> getPlayingStation(String ip, String port) async {
   Uri getPlayingStationPath =
-      Uri.parse(httpPrefix + ip + ':' + port + '/api/getPlayingStation/');
+      Uri.parse(httpPrefix + ip + ':' + port + '/api/radio/getPlayingStation/');
   final response = await http.get(getPlayingStationPath);
 
   if (response.statusCode == 200) {
@@ -30,7 +30,7 @@ Future<WebRadio> getPlayingStation(String ip, String port) async {
 
 Future<double> getVolume(String ip, String port) async {
   Uri getVolumePath =
-      Uri.parse(httpPrefix + ip + ':' + port + '/api/getVolume/');
+      Uri.parse(httpPrefix + ip + ':' + port + '/api/radio/getVolume/');
   final response = await http.get(getVolumePath);
 
   if (response.statusCode == 200) {
@@ -42,7 +42,7 @@ Future<double> getVolume(String ip, String port) async {
 
 Future<http.Response> setVolume(double volume, String ip, String port) {
   Uri setVolumePath =
-      Uri.parse(httpPrefix + ip + ':' + port + '/api/setVolume/');
+      Uri.parse(httpPrefix + ip + ':' + port + '/api/radio/setVolume/');
   String _volume = volume.round().toString();
   return http.post(setVolumePath,
       headers: <String, String>{
@@ -55,7 +55,7 @@ Future<http.Response> setVolume(double volume, String ip, String port) {
 
 Future<http.Response> playRadio(WebRadio radio, String ip, String port) {
   Uri playRadioPath =
-      Uri.parse(httpPrefix + ip + ':' + port + '/api/playRadio/');
+      Uri.parse(httpPrefix + ip + ':' + port + '/api/radio/playRadio/');
   return http.post(playRadioPath,
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
@@ -66,7 +66,7 @@ Future<http.Response> playRadio(WebRadio radio, String ip, String port) {
 
 Future<http.Response> stopRadio(String ip, String port) {
   Uri stopRadioPath =
-      Uri.parse(httpPrefix + ip + ':' + port + '/api/stopRadio/');
+      Uri.parse(httpPrefix + ip + ':' + port + '/api/radio/stopRadio/');
   return http.post(
     stopRadioPath,
     headers: <String, String>{
@@ -77,7 +77,7 @@ Future<http.Response> stopRadio(String ip, String port) {
 
 Future<http.Response> speakerVolumeUP(String ip, String port) {
   Uri speakerVolumeUPPath =
-      Uri.parse(httpPrefix + ip + ':' + port + '/api/speakerVolumeUP/');
+      Uri.parse(httpPrefix + ip + ':' + port + '/api/speaker/volumeUp/');
 
   return http.post(
     speakerVolumeUPPath,
@@ -89,7 +89,7 @@ Future<http.Response> speakerVolumeUP(String ip, String port) {
 
 Future<http.Response> speakerVolumeDOWN(String ip, String port) {
   Uri speakerVolumeUPPath =
-      Uri.parse(httpPrefix + ip + ':' + port + '/api/speakerVolumeDOWN/');
+      Uri.parse(httpPrefix + ip + ':' + port + '/api/speaker/volumeDown/');
 
   return http.post(
     speakerVolumeUPPath,
@@ -101,7 +101,7 @@ Future<http.Response> speakerVolumeDOWN(String ip, String port) {
 
 Future<http.Response> speakerVolumeOnOff(String ip, String port) {
   Uri speakerVolumeUPPath =
-      Uri.parse(httpPrefix + ip + ':' + port + '/api/speakerOnOff/');
+      Uri.parse(httpPrefix + ip + ':' + port + '/api/speaker/toggle/');
 
   return http.post(
     speakerVolumeUPPath,
